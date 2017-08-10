ea906c69bac0af4cd93601a7e4f79feb5bee4595
esaburruss <edward.burruss@messagegears.com>
2017-08-10 10:03:31 -0400

    Install.sh Worked

--- backend/src/install.sh
+++ backend/src/install.sh
@@ -1,10 +1,9 @@
+mysql -u root -p < ../initial_setup.sql
 rm ./content/migrations/*.py
 rm ./profiles/migrations/*.py
-python manage.py makemigrations
-cd ..
-mysql -u root -p < initial_setup.sql
-cp ./easy_install_files/0002_initial_setup_profile.py ./src/profiles/migrations
-cp ./easy_install_files/0002_initial_setup_content.py ./src/content/migrations
-cd ./src
+python manage.py makemigrations profiles
+python manage.py makemigrations content
+cp ../easy_install_files/0002_initial_setup_profile.py ./profiles/migrations
+cp ../easy_install_files/0002_initial_setup_content.py ./content/migrations
 python manage.py migrate
 echo 'Installation Completed'
